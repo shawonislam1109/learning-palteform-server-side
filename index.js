86e113f899b949b167f728cd27e75ffc3f216a12
shawonislam1109 <shawonislam1109@gmail.com>
2022-10-25 23:00:50 +0600

    category details data

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
-const cors = require('cors');
 const crouse = require('./Data/CrouseCategory.json')
 
+app.use(cors())
 
-
-app.use(cors());
 app.get('/', (req, res) => {
     res.send('yes this the connected')
 })
 
 app.get('/crouse_Data', (req, res) => {
     res.send(crouse)
+})
+app.get('/crouse_Data/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const singleData = crouse.filter(data => data.id == id);
+    res.send(singleData);
 })
 
 app.listen(port, () => {
