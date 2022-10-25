f290799347f6eb593238ff3e04be3c1ab48281b2
shawonislam1109 <shawonislam1109@gmail.com>
2022-10-25 20:43:40 +0600

    set in  index.js data

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
-const cors = require('cors')
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+const crouse = require('./Data/CrouseCategory.json')
+
+
 
 app.use(cors());
-
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('yes this the connected')
+})
+
+app.get('/crouse_Data', (req, res) => {
+    res.send(crouse)
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port`)
+    console.log('yes i am here connected')
 })
